1909a82529a74b3c62b54704acb9d86e7472ee14
Athul Muralidhar <athul.m68@gmail.com>
2021-12-01 22:57:39 +0100

    added loops

---
+++ getting-started/loops.go
@@ -0,0 +1,68 @@
+package main
+
+func Loops() {
+
+	// 1
+	// var i int
+	// for i < 5 {
+	// 	println(i) 
+	// 	i +=1
+
+	// 	if i == 1 {
+	// 		continue
+	// 	}
+	// 	fmt.Println("asdasd")
+	// }
+
+	// 2
+	// for i:= 0; i < 5; i++ {
+	// 	println(i)
+	// } 
+
+	// 3
+	// var i int
+	// for ; i < 5; i +=1 {
+	// 	println(i) 
+	// }
+
+	// 4
+	// var i int 
+	// for ; ; {
+	// 	if i == 5 {
+	// 		break
+	// 	}
+	// 	i++
+	// }
+
+	// 5
+	// var i int 
+	// for {
+	// 	if i == 5 { break }
+	// 	println(i)
+	// 	i++
+	// }
+
+	// slice := []int{1,2,3}
+
+	// for i,v := range slice {
+	// 	println(i, v)
+	// }
+
+
+	// somemap := map[string]int{"http":2, "tcp":4}
+	// for k,v := range somemap {
+	// 	println(k, v)
+	// }
+
+
+	// somemap := map[string]int{"http":2, "tcp":4}
+	// for k := range somemap {
+	// 	println(k)
+	// }
+	// for _, v := range somemap {
+	// 	println(v)
+	// }
+
+	panic("something happened")
+
+}
